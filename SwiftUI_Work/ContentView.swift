979ef50026072ea5e89cb217a09b6f8a507ae52b
yukiboard <yukiya062582@gmail.com>
2020-05-02 13:56:47 +0900

    first commit

---
+++ SwiftUI_Work/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Work
+//
+//  Created by 水越侑哉也 on 2020/05/02.
+//  Copyright © 2020 水越侑哉也. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
